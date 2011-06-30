e770c5ff6dcd0caa9652f8140cfa24f5c2d0cbdb
Caesar <caesarliu@gmail.com>
2011-06-30 22:45:49 +0800

    Shopping Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,15 +6,29 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class Product < ActiveRecord::Base
-  default_scope :order => 'title'
-  
-  validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-# 
-  validates :title, :uniqueness => true
-  validates :image_url, :format => {
+ class Product < ActiveRecord::Base
+    default_scope :order => 'title'
+
+    has_many :line_items, :class_name => "LineItem" 
+    before_destroy :ensure_not_referenced_by_any_line_item
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.count.zero?
+        return true
+      else
+        errors[:base] << "Line Items present"
+        return false
+      end
+    end
+
+    #
+
+    validates :title, :description, :image_url, :presence => true
+    validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+    #
+    validates :title, :uniqueness => true
+    validates :image_url, :format => {
     :with    => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
-end
+  end
