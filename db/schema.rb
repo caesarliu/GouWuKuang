f39bd3bdc9777d38d7d7e3fd1b3de863879ff81e
leon cheng <leoncheng0528@hotmail.com>
2011-07-09 09:44:44 +0800

    Revert dca17a782201988cc190957a83e98ce175bd9ed8^..HEAD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110610152931) do
+ActiveRecord::Schema.define(:version => 20110703141008) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
