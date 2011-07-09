3579e6d889dcb6c2fa38d114d9e725b2ebf47fc9
leon cheng <leoncheng0528@hotmail.com>
2011-07-09 09:50:25 +0800

    Revert "Revert dca17a782201988cc190957a83e98ce175bd9ed8^..HEAD"
    This reverts commit f39bd3bdc9777d38d7d7e3fd1b3de863879ff81e.

--- app/models/line_item.rb
+++
@@ -1,8 +0,0 @@
-class LineItem < ActiveRecord::Base
-  belongs_to :product
-  belongs_to :cart
-  
-  def total_price
-    product.price * quantity
-  end
-end
--- db/migrate/20110703134741_add_quantity_to_line_item.rb
+++
@@ -1,9 +0,0 @@
-class AddQuantityToLineItem < ActiveRecord::Migration
-  def self.up
-    add_column :line_items, :quantity, :integer, :default => 1
-  end
-
-  def self.down
-    remove_column :line_items, :quantity
-  end
-end
