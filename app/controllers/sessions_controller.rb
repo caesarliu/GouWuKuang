3a4d20b46150a10de9416b1b2140d90cac10824f
leon cheng <leoncheng0528@hotmail.com>
2011-07-27 22:53:59 +0800

    redirect to previous url after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
   skip_before_filter :authorize
   def new
+      session[:previous_url] = request.referer
+	  p session[:previous_url]
   end
 
   def create
 	if user = User.authenticate(params[:name], params[:password])
 		session[:user_id] = user.id
 		session[:user_name] = user.name
-		redirect_to request.referer
+		p "redirect to " + session[:previous_url]
+		redirect_to session[:previous_url]
+		
 	else
 		redirect_to login_url, :alert => "无效的用户名或密码";
 	end
